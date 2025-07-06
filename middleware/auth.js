f6f3ce96e8051303e0f9d77729722697bd0fed50
IlDavidone <144787644+IlDavidone@users.noreply.github.com>
2025-07-06 23:16:54 +0200

    Implemented authentication middleware for certain routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,34 @@
-module.exports.isAuth = (req, res, next) => {
-    if (req.user) {
-        next();
-    } else {
-        res.redirect("/");
-    }
-}
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const jwt = require("jsonwebtoken");
+const connection = require('../config/database/schemas');
+const User = connection.models.User;
 
-module.exports.isNotAuth = (req, res, next) => {
-    if (req.user) {
-        res.redirect("/chat");
-    } else {
-        next();
-    }
-}
+export const authenticatedRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
 
-module.exports.isAdmin = (req, res, next) => {
-    if (req.session.user && req.user.admin) {
+        if (!token) {
+            return res.status(401).json({message: "You are not authorized, no token provided"});
+        }
+
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+        if (!decodedToken) {
+            return res.status(401).json({message: "You are not authorized, invalid token provided"});
+        }
+
+        const user = await User.findById(decodedToken.userId).select("-passwordSalt -passwordHash");
+
+        if (!user) {
+            return res.status(404).json({message: "User not found"})
+        }
+
+        req.user = user;
+
         next();
-    } else {
-        res.status(401).json({ msg: 'You are not authorized to view this resource because you are not an admin.' });
+    } catch (err) {
+        console.log("An error occurred while verifying a token: ", err.message);
+        res.status(500).json({message: "Internal server error"});
     }
 }
--- routes/logout.js
+++ routes/logout.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const isAuth = require("../middleware/auth").isAuth;
 const { logout } = require("../controllers/auth.controller");
+const { authenticatedRoute } = require("../middleware/auth");
 
-router.get("/logout", (req, res, next) => {
+router.get("/logout", authenticatedRoute, (req, res, next) => {
   logout(req, res);
 });
 
